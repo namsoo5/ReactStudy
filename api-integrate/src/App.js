6e93f325a6365d30753a21f5f72f98dfc7340f09
namsoo5 <are34@naver.com>
2019-12-10 11:39:43 +0900

    api통신 연습

---
+++ api-integrate/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios'
+import Users from './Users';
+
+function App() {
+  return (
+    <Users/>
+  );
+}
+
+export default App;
